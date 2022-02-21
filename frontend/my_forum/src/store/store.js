058e24a3fdf70f89d85f07ea1591993c6b1299ae
Lungu Mihai Adrian <lunguadrian723@yahoo.co.uk>
2022-02-21 10:41:42 +0200

    added new files

--- frontend/my_forum/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from 'redux'
-import { usersReducer } from './users';
-import { eventsReducer } from './events';
-
-export const store = createStore(combineReducers({
-    users: usersReducer,
-    events: eventsReducer
-}));
